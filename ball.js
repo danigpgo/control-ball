011092e0e184f611b2df7fe9656d0b13e9d0d4b1
Danigp <danielentj@gmail.com>
2017-06-28 14:56:12 +0200

    safe

--- ball.js
+++ ball.js
@@ -1,7 +1,9 @@
 function Ball() {
   this.speed = 5;
-  this.direction = 3;
+  this.direction = 1;
   this._getPosition();
+  this._getPosition2();
+  this._getPosition3();
   this._move();
   this.DIAMETER = 45;
 }
@@ -18,6 +20,28 @@ Ball.prototype._getPosition = function() {
   };
 };
 
+Ball.prototype._getPosition2 = function() {
+  this.ballPosition = {
+    top: $("#ball").position().top,
+    left: $("#ball").position().left
+  };
+  this.arrowPosition2 = {
+    top: $("#arrow2").position().top,
+    left: $("#arrow2").position().left,
+  };
+};
+
+Ball.prototype._getPosition3 = function() {
+  this.ballPosition = {
+    top: $("#ball").position().top,
+    left: $("#ball").position().left
+  };
+  this.arrowPosition3 = {
+    top: $("#arrow3").position().top,
+    left: $("#arrow3").position().left,
+  };
+};
+
 Ball.prototype._move = function() {
   var that = this;
   var intervalId = setInterval(function() {
@@ -28,22 +52,34 @@ Ball.prototype._move = function() {
           left: that.ballPosition.left += that.speed / 2,
         });
         if (that._ballCollidesScreenRight()) {
-          that._wallCollision();
+          that.direction = 3;
         }
-        if (that._ballCollidesArrowRight()) {
-          that._toolCollision();
+        else if (that._ballCollidesArrowRight()) {
+          that.direction = 4;
+        }
+        else if (that._ballCollidesArrowRight2()) {
+          that.direction = 3;
+        }
+        else if (that._ballCollidesArrowRight3()) {
+          that.direction = 2;
         }
         break;
       case 2:
       // MOVER ABAJO //
         $("#ball").css({
-          left: that.ballPosition.top += that.speed / 2,
+          top: that.ballPosition.top += that.speed / 2,
         });
         if (that._ballCollidesScreenDown()) {
-          that._wallCollision();
+          that.direction = 4;
         }
-        if (that._ballCollidesArrowDown()) {
-          that._toolCollision();
+        else if (that._ballCollidesArrowDown()) {
+          that.direction = 4;
+        }
+        else if (that._ballCollidesArrowDown2()) {
+          that.direction = 3;
+        }
+        else if (that._ballCollidesArrowDown3()) {
+          that.direction = 2;
         }
         break;
       case 3:
@@ -52,22 +88,34 @@ Ball.prototype._move = function() {
         left: that.ballPosition.left -= that.speed,
         });
         if (that._ballCollidesScreenLeft()) {
-          that._wallCollision();
+          that.direction = 1;
         }
-        if (that._ballCollidesArrowLeft()) {
-          that._toolCollision();
+        else if (that._ballCollidesArrowLeft()) {
+          that.direction = 4;
+        }
+        else if (that._ballCollidesArrowLeft2()) {
+          that.direction = 3;
+        }
+        else if (that._ballCollidesArrowLeft3()) {
+          that.direction = 2;
         }
         break;
       case 4:
       // MOVER ARRIBA //
         $("#ball").css({
-          left: that.ballPosition.top -= that.speed / 2,
+          top: that.ballPosition.top -= that.speed / 2,
         });
         if (that._ballCollidesScreenUp()) {
-          Ball._wallCollision();
+          that.direction = 2;
         }
-        if (that._ballCollidesArrowUp()) {
-          Ball._toolCollision();
+        else if (that._ballCollidesArrowUp()) {
+          that.direction = 4;
+        }
+        else if (that._ballCollidesArrowUp2()) {
+          that.direction = 3;
+        }
+        else if (that._ballCollidesArrowUp3()) {
+          that.direction = 2;
         }
         break;
     }
@@ -85,7 +133,6 @@ Ball.prototype._ballCollidesArrowRight = function() {
 
 Ball.prototype._ballCollidesArrowLeft = function() {
   this._getPosition();
-
   return this.ballPosition.left > this.arrowPosition.left + 40 &&
   this.ballPosition.left < this.arrowPosition.left + 65 &&
   this.ballPosition.top > this.arrowPosition.top &&
@@ -94,7 +141,6 @@ Ball.prototype._ballCollidesArrowLeft = function() {
 
 Ball.prototype._ballCollidesArrowDown = function() {
   this._getPosition();
-
   return this.ballPosition.top + this.DIAMETER > this.arrowPosition.top &&
   this.ballPosition.top < this.arrowPosition.top -20 &&
   this.ballPosition.left > this.arrowPosition.left &&
@@ -103,72 +149,93 @@ Ball.prototype._ballCollidesArrowDown = function() {
 
 Ball.prototype._ballCollidesArrowUp = function() {
   this._getPosition();
-
   return this.ballPosition.top < this.arrowPosition.top + 55 &&
   this.ballPosition.top > this.arrowPosition.top +65 &&
   this.ballPosition.left > this.arrowPosition.left &&
   this.ballPosition.left < this.arrowPosition.left + 55;
 };
 
-Ball.prototype._ballCollidesScreenRight = function() {
-  this._getPosition();
 
-  return this.ballPosition.left > 1238;
+
+Ball.prototype._ballCollidesArrowRight2 = function() {
+  this._getPosition2();
+  return this.ballPosition.left + this.DIAMETER < this.arrowPosition2.left &&
+    this.ballPosition.left + this.DIAMETER > this.arrowPosition2.left -5 &&
+    this.ballPosition.top > this.arrowPosition2.top &&
+    this.ballPosition.top < this.arrowPosition2.top + 55;
+};
+
+Ball.prototype._ballCollidesArrowLeft2 = function() {
+  this._getPosition2();
+  return this.ballPosition.left > this.arrowPosition2.left + 40 &&
+  this.ballPosition.left < this.arrowPosition2.left + 65 &&
+  this.ballPosition.top > this.arrowPosition2.top &&
+  this.ballPosition.top < this.arrowPosition2.top + 55;
+};
+
+Ball.prototype._ballCollidesArrowDown2 = function() {
+  this._getPosition2();
+  return this.ballPosition.top + this.DIAMETER > this.arrowPosition2.top &&
+  this.ballPosition.top < this.arrowPosition2.top -20 &&
+  this.ballPosition.left > this.arrowPosition2.left &&
+  this.ballPosition.left < this.arrowPosition2.left + 55;
+};
+
+Ball.prototype._ballCollidesArrowUp2 = function() {
+  this._getPosition2();
+  return this.ballPosition.top < this.arrowPosition2.top + 55 &&
+  this.ballPosition.top > this.arrowPosition2.top +65 &&
+  this.ballPosition.left > this.arrowPosition2.left &&
+  this.ballPosition.left < this.arrowPosition2.left + 55;
+};
+
+
+Ball.prototype._ballCollidesArrowRight3 = function() {
+  this._getPosition3();
+  return this.ballPosition.left + this.DIAMETER < this.arrowPosition3.left &&
+    this.ballPosition.left + this.DIAMETER > this.arrowPosition3.left -5 &&
+    this.ballPosition.top > this.arrowPosition3.top &&
+    this.ballPosition.top < this.arrowPosition3.top + 55;
+};
+
+Ball.prototype._ballCollidesArrowLeft3 = function() {
+  this._getPosition3();
+  return this.ballPosition.left > this.arrowPosition3.left + 40 &&
+  this.ballPosition.left < this.arrowPosition3.left + 65 &&
+  this.ballPosition.top > this.arrowPosition3.top &&
+  this.ballPosition.top < this.arrowPosition3.top + 55;
+};
+
+Ball.prototype._ballCollidesArrowDown3 = function() {
+  this._getPosition3();
+  return this.ballPosition.top + this.DIAMETER > this.arrowPosition3.top &&
+  this.ballPosition.top < this.arrowPosition3.top -20 &&
+  this.ballPosition.left > this.arrowPosition3.left &&
+  this.ballPosition.left < this.arrowPosition3.left + 55;
+};
+
+Ball.prototype._ballCollidesArrowUp3 = function() {
+  this._getPosition3();
+  return this.ballPosition.top < this.arrowPosition3.top + 55 &&
+  this.ballPosition.top > this.arrowPosition3.top +65 &&
+  this.ballPosition.left > this.arrowPosition3.left &&
+  this.ballPosition.left < this.arrowPosition3.left + 55;
+};
+
+
+Ball.prototype._ballCollidesScreenRight = function() {
+  return this.ballPosition.left > 1215;
 };
 
 Ball.prototype._ballCollidesScreenLeft = function() {
-  this._getPosition();
-
   return this.ballPosition.left < 0;
 };
 
 Ball.prototype._ballCollidesScreenDown = function() {
-  this._getPosition();
-
-  return this.ballPosition.top > 380;
+  return this.ballPosition.top > 370;
 };
 
 
 Ball.prototype._ballCollidesScreenUp = function() {
-  this._getPosition();
-
   return this.ballPosition.top < 1;
-};
-
-
-Ball.prototype._wallCollision = function() {
-  var that = this;
-  switch (that.direction) {
-    case 1:
-      that.direction = 3;
-      break;
-    case 2:
-      that.direction = 4;
-      break;
-    default:
-    case 3:
-      that.direction = 1;
-      break;
-    case 4:
-      that.direction = 2;
-      break;
-  }
-};
-Ball.prototype._toolCollision = function() {
-  var that = this;
-    switch (that.direction) {
-      case 1:
-        that.direction = 3;
-        break;
-      case 2:
-        that.direction = 4;
-        break;
-      case 3:
-        that.direction = 1;
-        break;
-      case 4:
-        that.direction = 2;
-        break;
-      default:
-    }
 };
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var ball = new Ball();
 var time = new TimerCount();
+var arrows = new Arrows();
